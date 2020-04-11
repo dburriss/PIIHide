a3aad1ab11f8850ba73eb60564d28685c639e1b1
Devon Burriss <devon@chimplab.co>
2020-04-11 23:26:10 +0200

    Implements a converter on attribute

--- test/PIIHide.Tests.Unit/PIITests.fs
+++ test/PIIHide.Tests.Unit/PIITests.fs
@@ -1,16 +1,19 @@
 module PIITests
 
+open System
 open System.Diagnostics
 open PIIHide
 open Xunit
 open Swensen.Unquote
 
-type Purchase() =
-    member val OrderNumber = "" with get, set
-    [<PII>] member val CustomerName = "" with get, set
+type Customer() =
+    member val CustomerNumber = Guid.NewGuid() with get, set
+    member val Reference = "" with get, set
+    [<PII>] member val Name = "" with get, set
     [<PII>] member val Address = "" with get, set
+    //[<PII>] member val Birth = DateTimeOffset.MinValue with get, set
 
-let aSimpleClass() = Purchase(OrderNumber="not pi", CustomerName = "hide me")
+let aSimpleClass() = Customer(Name = "hide me")
 let aKey() = Encryption.makeKey()
 
 [<Fact>]
@@ -18,24 +21,24 @@ let ``hide encrypts all properties with PII attribute on simple string members``
     let o = aSimpleClass()
     let key = aKey()
     let enc = o |> PII.hide key
-    test <@ enc.OrderNumber |> String.startsWith "ENC:" |> not @>
-    test <@ enc.CustomerName |> String.startsWith "ENC:" @>
+    test <@ enc.Reference |> String.startsWith "ENC:" |> not @>
+    test <@ enc.Name |> String.startsWith "ENC:" @>
     
 [<Fact>]
 let ``show decrypts all properties with PII attribute on simple string members`` () =
     let o = aSimpleClass()
     let key = aKey()
     PII.hide key o |> ignore
-    test <@ o.CustomerName |> String.startsWith "ENC:" @>
+    test <@ o.Name |> String.startsWith "ENC:" @>
     PII.show key o |> ignore
-    test <@ o.CustomerName = (aSimpleClass()).CustomerName @>
+    test <@ o.Name = (aSimpleClass()).Name @>
     
 [<Fact>]
 let ``all string PII members are encrypted`` () =
     let o = aSimpleClass()
     let key = aKey()
     PII.hide key o |> ignore
-    test <@ o.CustomerName |> String.startsWith "ENC:" @>
+    test <@ o.Name |> String.startsWith "ENC:" @>
     test <@ o.Address |> String.startsWith "ENC:" @>
     
 [<Fact>]
@@ -49,3 +52,12 @@ let ``encrypt and decrypt 1 thousand simple objects in under 50ms`` () =
         
     watch.Stop()
     test <@ watch.ElapsedMilliseconds < 50L @>
+    
+[<Fact>]
+let ``DefaultConverter can convert strings`` () =
+    let converter = DefaultConverter() :> IStringConverter
+    let s = "test"
+    let o = "test" :> obj
+    test <@ converter.CanConvert(typeof<string>) = true @>
+    test <@ converter.FromAString(s) = o @>
+    test <@ converter.ToAString(o) = s @>
