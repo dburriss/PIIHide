cf318228f481732c432b51be5f54c1b5bc5f6c0d
Devon Burriss <devon@chimplab.co>
2020-04-14 16:02:00 +0200

    Adds a CSharp sample application

---
+++ samples/csharp/ConsoleApp/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.Json;
+using PIIHide;
+using Encryption = PIIHide.CSharp.Encryption;
+using PIIHide.CSharp.Extensions;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = Encryption.MakeKey();
+            var person = MakePerson();
+            PrintPerson(person, "A PERSON MODEL", ConsoleColor.Red);
+            person.Encrypt(key);
+            PrintPerson(person, "ENCRYPTED PERSON", ConsoleColor.Green);
+            person.Decrypt(key);
+            PrintPerson(person, "DECRYPTED PERSON", ConsoleColor.Red);
+        }
+        
+        static Person MakePerson()
+        {
+            return new Person
+            {
+                Id = 1,
+                Name = "Bob Builder",
+                Address = new Address
+                {
+                    Street = "42 Quarry Lane",
+                    PostalCode = "8U1LD3R",
+                    Country = "England"
+                }
+            };
+        }
+
+        static void PrintPerson(Person person, string title, ConsoleColor color)
+        {
+            var jsonPerson = JsonSerializer.Serialize(person, JsonOptions);
+            var curColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine($"===== {title} =====");
+            Console.WriteLine(jsonPerson);
+            Console.ForegroundColor = curColor;
+        }
+        
+        private static JsonSerializerOptions JsonOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+    }
+
+    public class Person
+    {
+        public long Id { get; set; }
+        [PII]
+        public string Name { get; set; }
+
+        [PII]
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        [PII]
+        public string Street { get; set; }
+
+        [PII]
+        public string PostalCode { get; set; }
+
+        public string Country { set; get; }
+    }
+}
